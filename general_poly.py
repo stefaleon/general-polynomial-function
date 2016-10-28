7d49a49893e624047d029e2a066441fe20b0b1dc
maimous@gmail.com <stefaleon@gmail.com>
2016-10-29 01:56:08 +0300

    tidy up

--- general_poly.py
+++ general_poly.py
@@ -1,25 +1,50 @@
 def general_poly (L):
+    
     """ L, a list of numbers (n0, n1, n2, ... nk)
     Returns a function, which when applied to a value x, returns the value 
     n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
+
     import math
 
-    def polynomial(x):
+    def calculate_polynomial(base):
+
         length = len(L)
         sum = 0
+
         for i in range(0, length):
             k = length-1-i;
-            sum += L[i]*math.pow(x, k)
+            sum += L[i]*math.pow(base, k)
+
         if sum.is_integer():
             return int(sum)
         else:
             return sum
+
     
-    return polynomial
+    return calculate_polynomial
 
 
 
-L=[1,2,3,4]
-print(len(L))
 
-print(general_poly(L)(10))
+# test cases
+
+L= [1,2,3,4]
+base = 10
+print(general_poly(L)(base))
+
+L= [5,0]
+base = 16
+print(general_poly(L)(base))
+
+L= [1,0,1,1]
+base = 2
+print(general_poly(L)(base))
+
+
+L= [1,2,3,4]
+base = 1.3
+print(general_poly(L)(base))
+
+L= [11,12,13]
+base = 0.5
+print(general_poly(L)(base))
